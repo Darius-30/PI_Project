0abd29f2f01a50546136f7afd9bf71949b2767f8
Darius.30 <HDarius-Marian@proton.me>
2025-11-27 18:29:54 +0200

    ADDED FORK - multiprocessing

--- client_ipv6.c
+++ client_ipv6.c
@@ -24,7 +24,7 @@ int get_page(char *address, char **response, int *response_size){
     size_t header_size = 0;
     
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET6;
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     printf("Obtinem adresa IPV6 a server-ului %s...\n", address);
     status = getaddrinfo(address, "http", &hints, &res);
