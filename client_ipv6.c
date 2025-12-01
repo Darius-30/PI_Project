a204e0391f1e7e9b649619fbc63800961809ceb9
Darius.30 <HDarius-Marian@proton.me>
2025-12-02 01:19:40 +0200

    ADDED COMMENTS

--- client_ipv6.c
+++ client_ipv6.c
@@ -10,12 +10,9 @@
 #include <openssl/err.h>
 
 int get_page(char *address, char **response, int *response_size){
-
-	SSL_library_init();
     
-    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
-    SSL *ssl = SSL_new(ctx);
-
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method()); // Initializam contextul SSL (ca si client)
+    SSL *ssl = SSL_new(ctx); // Cream o noua structura SSL folosind contextul creat anterior
 
     int status;
     struct addrinfo hints;
@@ -34,33 +31,46 @@ int get_page(char *address, char **response, int *response_size){
     
 
 	
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    printf("Obtinem adresa IPV6 a server-ului %s...\n", address);
-    status = getaddrinfo(address, "https", &hints, &res);
-    if(status != 0){
+    memset(&hints, 0, sizeof hints); // Initializam structura hints pentru getaddrinfo - cu 0
+    hints.ai_family = AF_INET6; // Specificam ca dorim adrese IPV6
+    hints.ai_socktype = SOCK_STREAM; // Specificam ca dorim socket de tip stream (TCP)
+    printf("Obtinem adresa IPV6 a server-ului %s...\n", address); 
+    status = getaddrinfo(address, "https", &hints, &res); // Obtinem adresa IPV6 a server-ului
+    if(status != 0){ // Verificam daca getaddrinfo a avut succes
         printf("Error: %s", gai_strerror(status));
         return -1;
     }
-    getnameinfo(res->ai_addr, res->ai_addrlen, host, sizeof host, NULL, 0, NI_NUMERICHOST);
+
+    /**
+     * getnameinfo:
+     * - res->ai_addr: adresa socketului rezultată din rezolvarea numelui (struct sockaddr) care va fi convertită.
+     * - res->ai_addrlen: lungimea structurii res->ai_addr, necesară pentru a limita citirea corectă a memoriei.
+     * - host: bufferul unde va fi scrisă reprezentarea textuală (numerică) a adresei IP obținute.
+     * - sizeof host: dimensiunea bufferului host, pentru a preveni depășirile de memorie.
+     * - NULL: pointer pentru numele portului; NULL indică faptul că nu dorim să obținem această informație.
+     * - 0: lungimea bufferului destinat numelui portului, nefolosit deoarece cel anterior este NULL.
+     * - NI_NUMERICHOST: flag care specifică faptul că dorim adresa în formă numerică (fără rezolvare inversă DNS).
+     */
+    getnameinfo(res->ai_addr, res->ai_addrlen, host, sizeof host, NULL, 0, NI_NUMERICHOST); // Convertim adresa obtinuta intr-un string 
     printf("Adresa IPV6 a serverului %s este: %s\n", address, host);
-    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol); // Cream socketul
     if(sock == -1 ){
         fprintf(stderr, "Eroarea la crearea socketului: %s\n", strerror(errno));
-        freeaddrinfo(res);
+        freeaddrinfo(res); // Eliberam memoria alocata pentru adresa
         return -1;
     }
     printf("Socketul a fost creat cu succes!\n");
-    int conn = connect(sock, res->ai_addr, res->ai_addrlen);
+
+    int conn = connect(sock, res->ai_addr, res->ai_addrlen); // Ne conectam la server
     if(conn == -1){
         fprintf(stderr, "Eroare la conectare: %s\n", strerror(errno));
         close(sock);
-        freeaddrinfo(res);
+        freeaddrinfo(res); // Eliberam memoria alocata pentru adresa
         return -1;
     }
     printf("Conexiunea a fost stabilita!\n");
-    SSL_set_fd(ssl, sock);
+
+    SSL_set_fd(ssl, sock); // Asociem socketul cu structura SSL
 
 	if(SSL_connect(ssl) == -1){
         fprintf(stderr, "Eroare la SSL_connect: %s\n", ERR_error_string(SSL_get_error(ssl, -1), NULL));
@@ -70,7 +80,7 @@ int get_page(char *address, char **response, int *response_size){
     }
 
 
-    bytes_sent = SSL_write(ssl, request, len);
+    bytes_sent = SSL_write(ssl, request, len); // Trimitem cererea HTTP catre server - cu SSL_write, care cripteaza datele inainte de trimitere
     if(bytes_sent == -1){
         fprintf(stderr, "Eroare la trimiterea datelor: %s\n", strerror(errno));
         close(sock);
@@ -78,42 +88,43 @@ int get_page(char *address, char **response, int *response_size){
         return -1;
     }
     printf("S-au trimis %d bytes catre server\n", bytes_sent);
-    while((bytes_received = SSL_read(ssl, buffer, sizeof buffer - 1)) > 0){
-        char *new_resp = realloc(*response, total_size + bytes_received + 1);
+    while((bytes_received = SSL_read(ssl, buffer, sizeof buffer - 1)) > 0){ // Primim raspunsul de la server - cu SSL_read, care decripteaza datele primite
+        char *new_resp = realloc(*response, total_size + bytes_received + 1); // Realocam memoria pentru raspunsul complet
         if(!new_resp){
             printf("Eroarea la alocarea memoriei\n");
-            break;
             close(sock);
             freeaddrinfo(res);
             return -1;
         }
         *response = new_resp;
-        memcpy(*response + total_size, buffer, bytes_received);
-        total_size += bytes_received;
-        (*response)[total_size] = '\0';
+        memcpy(*response + total_size, buffer, bytes_received); // Copiem datele primite in raspunsul complet
+        total_size += bytes_received; // Actualizam dimensiunea totala a raspunsului
+        (*response)[total_size] = '\0'; // Adaugam terminatorul de sir
     }
-    FILE *file = fopen("index.html", "w");
+    FILE *file = fopen("index.html", "w"); // Deschidem fisierul pentru scriere
     if(file == NULL){
         fprintf(stderr, "Eroare la deschiderea fisierului: %s\n", strerror(errno));
         close(sock);
-        freeaddrinfo(res);
+        freeaddrinfo(res); // Eliberam memoria alocata pentru adresa
         return -1;
     }
         
-	char *html_body = strstr(*response, "\r\n\r\n");
+	char *html_body = strstr(*response, "\r\n\r\n"); // Cautam inceputul corpului HTML in raspuns
 	if(html_body != NULL){
-		html_body += 4;
-		header_size = html_body - *response;
-		body_size = total_size - header_size;
-        fwrite(html_body, 1, body_size, file);
-        fclose(file);
+		html_body += 4; // Sarim peste delimitatorul dintre header si body
+		header_size = html_body - *response;// Calculam dimensiunea header-ului
+		body_size = total_size - header_size;// Calculam dimensiunea corpului HTML
+        fwrite(html_body, 1, body_size, file); // Scriem corpul HTML in fisier
+        fclose(file); // Inchidem fisierul
         printf("Pagina salvata in index.html\n");
     }
-    memmove(*response, html_body, body_size);
-    *response_size = body_size;
+    memmove(*response, html_body, body_size); // Mutam corpul HTML la inceputul raspunsului
+    *response_size = body_size; // Actualizam dimensiunea raspunsului
     printf("S-au primit %zu bytes de la server\n", body_size);
-    close(sock);
-    freeaddrinfo(res);
+    close(sock); // Inchidem socketul
+    freeaddrinfo(res); // Eliberam memoria alocata pentru adresa
+    SSL_free(ssl); // Eliberam structura SSL
+    SSL_CTX_free(ctx); // Eliberam contextul SSL
     return 0;
 }
 
--- server_ipv4.c
+++ server_ipv4.c
@@ -22,28 +22,28 @@ int main(){
     char receive_buffer[256];
     int bytes_received;
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    memset(&hints, 0, sizeof hints); // Initializam structura hints pentru getaddrinfo - cu 0
+    hints.ai_family = AF_INET; // Specificam ca dorim adrese IPV4
+    hints.ai_socktype = SOCK_STREAM; // Specificam ca dorim socket de tip stream (TCP)
+    hints.ai_flags = AI_PASSIVE; // Specificam ca dorim sa ascultam pe toate interfetele disponibile 
 
     if(getaddrinfo(NULL, PORT, &hints, &res) != 0){
         fprintf(stderr, "Eroare la getaddrinfo: %s\n", gai_strerror(errno));
         return 1;
     }
 
-    sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol); // Cream socketul
     if(sock == -1){
         fprintf(stderr, "Eroare la crearea socketului: %s\n", strerror(errno));
         return 1;
     }
 
-    if(bind(sock, res->ai_addr, res->ai_addrlen) == -1){
+    if(bind(sock, res->ai_addr, res->ai_addrlen) == -1){ // Facem bind la socket (asociem socketul cu adresa si portul)
         fprintf(stderr, "Eroare la bind: %s\n", strerror(errno));
         return 1;
     }
 
-    if(listen(sock, MAX_PENDING) == -1){
+    if(listen(sock, MAX_PENDING) == -1){ // Punem socketul in stare de ascultare
         fprintf(stderr, "Eroare la listen: %s\n", strerror(errno));
         return 1;
     }
@@ -51,43 +51,43 @@ int main(){
     addr_size = sizeof their_addr;
     
     while(1){
-        int new_fd = accept(sock, (struct sockaddr *)&their_addr, &addr_size);
-        if(new_fd == -1){
+        int new_fd = accept(sock, (struct sockaddr *)&their_addr, &addr_size); // Acceptam o conexiune noua de la un client
+        if(new_fd == -1){ // Verificam daca accept a avut loc succes
             fprintf(stderr, "Eroare la accept: %s\n", strerror(errno));
             continue;
         }
         printf("S-a acceptat o conexiune noua de la adresa \n");
         
-        pid_t pid = fork();
-        if(pid == -1){
+        pid_t pid = fork(); // Cream un proces copil pentru a gestiona conexiunea cu clientul
+        if(pid == -1){ // Verificam daca fork a avut loc succes
             fprintf(stderr, "Eroare la fork: %s\n", strerror(errno));
             close(new_fd);
             continue;
         }
-        if(pid == 0){
-            close(sock);
-            while(1){
-                bytes_received = recv(new_fd, receive_buffer, sizeof receive_buffer - 1, 0);
-                if (bytes_received > 0) {
-                    receive_buffer[bytes_received] = '\0';
-                    printf("Am primit de la client: %s\n", receive_buffer);
-                    if(strcmp(receive_buffer, "07#") == 0){
-                        char *response = NULL;
+        if(pid == 0){ // Procesul copil gestioneaza conexiunea cu clientul
+            close(sock); // Inchidem socketul principal in procesul copil
+            while(1){ // Bucla pentru a primi si procesa date de la client
+                bytes_received = recv(new_fd, receive_buffer, sizeof receive_buffer - 1, 0); // Primim date de la client
+                if (bytes_received > 0) { // Verificam daca recv a avut succes
+                    receive_buffer[bytes_received] = '\0'; // Adaugam terminatorul de sir
+                    printf("Am primit de la client: %s\n", receive_buffer); // Afisam datele primite
+                    if(strcmp(receive_buffer, "07#") == 0){ // Daca comanda este 07#
+                        char *response = NULL; 
                         int response_size = 0;
-                        if(get_page(ADDRESS, &response, &response_size) == 0){
-                            send(new_fd, response, response_size, 0);
-                            free(response);
+                        if(get_page(ADDRESS, &response, &response_size) == 0){ // Obtinem pagina de la serverul web folosind functia din client_ipv6.c
+                            send(new_fd, response, response_size, 0); // Trimitem raspunsul inapoi la client
+                            free(response); // Eliberam memoria alocata pentru raspuns
                         } else {
                             char *error_msg = "Eroare la obtinerea paginii.\n";
-                            send(new_fd, error_msg, strlen(error_msg), 0);
+                            send(new_fd, error_msg, strlen(error_msg), 0); // Trimitem mesaj de eroare la client daca obtinerea paginii a esuat
                         }
                     }
                     else{
                         char *msg = "Comanda necunoscuta.\n";
-                        send(new_fd, msg, strlen(msg), 0);
+                        send(new_fd, msg, strlen(msg), 0); // Trimitem mesaj de eroare la client daca comanda este necunoscuta
                     }
                 } else if (bytes_received == 0) {
-                    printf("Clientul a închis conexiunea.\n");
+                    printf("Clientul a închis conexiunea.\n"); // Clientul a închis conexiunea
                     break;
                 } else {
                     perror("Eroare la recv");
